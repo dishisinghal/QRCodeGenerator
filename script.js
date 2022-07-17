b48313a7a8828c7a20ab0424a31d28d211f34640
dishisinghal <60167323+dishisinghal@users.noreply.github.com>
2022-07-17 18:15:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let input=document.querySelector(".form input");
+let button=document.querySelector(".form button");
+let qrImg=document.querySelector(".qr-img img");
+
+button.addEventListener("click",()=>{
+    let data=input.value;
+    if(data.length>0)
+    {
+      let imgSrc= "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data="+data;
+      qrImg.src = imgSrc;
+    }
+    else
+    {
+      alert('Enter a valid text or URL');
+    }
+  })
